689c5ada488c9a91f3318fe4b3f934714d091805
Disanya <sithuli.20210198@iit.ac.lk>
2023-04-05 09:42:05 +0530

    New changes

---
+++ test/unit_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mate/speech_screen.dart';
+
+void main() {
+  test("recording the voice", () {
+    SpeechScreen speechScreen = const SpeechScreen();
+    expect(0, speechScreen);
+  });
+}
